55884a6e4cf75dfd8b837ff492bea0092feef045
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-22 13:47:38 +0600

    Our Recipes Part done

--- src/components/OurRecipes.jsx
+++ src/components/OurRecipes.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const OurRecipes = () => {
     return (
-        <div>
-            Our Recipes
+        <div className='text-center my-8 w-8/12 mx-auto space-y-4'>
+            <h1 className='text-5xl font-semibold text-gray-800'>Our Recipes</h1>
+            <p className='text-base text-gray-600'>Explore our diverse range of recipes, featuring delicious and easy-to-follow dishes for every meal. Whether you’re a seasoned chef or a kitchen novice, you’ll find something to suit your taste. Join us in celebrating culinary creativity and discover new favorites that will delight your family and friends!</p>
         </div>
     );
 };
